48fe8d07827f4c6f7a087fa23871123c892cc7a6
thomas <Thomas.Schroedter@gmx.net>
2018-01-11 16:51:43 +1100

    Core2: Added Core2.Selkie.Aop.Example

---
+++ Core2.Selkie.Aop.Example/Installer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Castle.MicroKernel.Registration;
+using Core2.Selkie.Windsor;
+
+namespace Core2.Selkie.Aop.Example
+{
+    [ExcludeFromCodeCoverage]
+    public class Installer
+        : BasicConsoleInstaller,
+          IWindsorInstaller
+    {
+        public override bool IsAutoDetectAllowedForAssemblyName(string assemblyName)
+        {
+            return assemblyName.StartsWith("Core2.Selkie.",
+                                           StringComparison.InvariantCulture);
+        }
+    }
+}
---
+++ Core2.Selkie.Aop.Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Castle.Windsor;
+
+namespace Core2.Selkie.Aop.Example
+{
+    [ExcludeFromCodeCoverage]
+    public class Program
+    {
+        public static void Main()
+        {
+            var container = new WindsorContainer();
+            var installer = new Installer();
+
+            var example = new Example(container,
+                                      installer);
+
+            example.Main();
+
+            Console.ReadLine();
+
+            example.Dispose();
+
+            Environment.Exit(0);
+        }
+    }
+}
