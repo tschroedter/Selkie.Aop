7388ffde1af08292c30e58e304b25a5fc6df9eb4
thomas <Thomas.Schroedter@gmx.net>
2018-01-11 16:45:19 +1100

    Core2: Added Core2.Selkie.Aop.Example.Data project

---
+++ Core2.Selkie.Aop.Example.Data/ISomething.cs
@@ -0,0 +1,15 @@
+﻿namespace Core2.Selkie.Aop.Example.Data
+{
+    public interface ISomething
+    {
+        int Property { get; set; }
+
+        int Augment(int input);
+        void DoSomething(string input);
+
+        void DoSomething(Record record);
+        void DoSomethingStatus(string input);
+        void DoSomethingStatus(Record record);
+        void DoSomethingThrows(Record record);
+    }
+}
---
+++ Core2.Selkie.Aop.Example.Data/Something.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Castle.Core;
+using Core2.Selkie.Aop.Aspects;
+using Core2.Selkie.Windsor;
+
+namespace Core2.Selkie.Aop.Example.Data
+{
+    [ExcludeFromCodeCoverage]
+    [Interceptor(typeof( StatusAspect ))]
+    [Interceptor(typeof( LogAspect ))]
+    [Interceptor(typeof( MessageHandlerAspect ))]
+    [ProjectComponent(Lifestyle.Transient)]
+    public class Something : ISomething
+    {
+        public int Augment(int input)
+        {
+            return input + 1;
+        }
+
+        public void DoSomething(string input)
+        {
+            Console.WriteLine("I'm doing something: " + input);
+        }
+
+        public void DoSomething(Record record)
+        {
+            Console.WriteLine(record);
+        }
+
+        public void DoSomethingThrows(Record record)
+        {
+            Console.WriteLine("I'm doing something (record) and throw: " + record);
+            throw new ArgumentException("Test");
+        }
+
+        [Status("**** Doing something (status)... ***")]
+        public void DoSomethingStatus(string input)
+        {
+            Console.WriteLine("I'm doing something (status): " + input);
+        }
+
+        [Status("+++ Doing something (status)... +++")]
+        public void DoSomethingStatus(Record record)
+        {
+            Console.WriteLine(record);
+        }
+
+        public int Property { get; set; }
+    }
+}
